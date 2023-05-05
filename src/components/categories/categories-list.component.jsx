c573fc9e76d94fcf0be71ada87844660e0845384
ros1440 <ros1440@northwesternmutual.com>
2023-05-05 14:55:02 -0500

    test

--- src/components/categories/categories-list.component.jsx
+++ src/components/categories/categories-list.component.jsx
@@ -4,6 +4,7 @@ import "./categories-list.style.scss";
 const CategoriesList = ({ categories }) => {
   return (
     <div className="categories-container">
+      <h1>hello</h1>
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
